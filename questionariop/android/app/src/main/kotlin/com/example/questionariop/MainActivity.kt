b74fe9c43a0d47a99d06beb79538cdde4475fa52
Akira <bruno.ram2004@gmail.com>
2023-06-12 15:05:46 -0300

    jhjhj

---
+++ questionariop/android/app/src/main/kotlin/com/example/questionariop/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.questionariop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ questionariop/lib/main.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+import './questao.dart';
+import './resposta.dart';
+
+main() {
+  runApp(AulaComponentes());
+}
+
+class AulaComponentes extends StatefulWidget{
+  @override
+  State<AulaComponentes> createState() => _AulaComponentesState();
+}
+
+class _AulaComponentesState extends State<AulaComponentes> {
+
+  var perguntaAtual = 0;
+  var cor = Colors.white;
+
+
+  final List<Map<String, Object>> perguntas = [
+    {
+      "texto" : "Qual a sua cor favorita?",
+      "respostas" : ["Amarelo","Preto", "Branco", "Azul", "Vermelho"]
+    },
+    {
+      "texto" : "Qual é seu animal favorito?",
+      "respostas" : ["Cachorro", "Gato", "Tartaruga", "Periquito"]
+    },
+    {
+      "texto" : "Qual sua linguagem favorita?",
+      "respostas" : ["Python", "Java", "JavaScript"]
+    },
+
+  ];
+  
+   bool get tempergunta{
+
+
+    return perguntaAtual < perguntas.length;
+
+   }
+
+
+
+  void acao(){
+    setState(() {
+      perguntaAtual++;
+    });
+    print(perguntaAtual);
+  }
+
+
+  Widget build(BuildContext context){
+
+    List<Widget> respostas = [];
+   
+if (tempergunta){
+
+ for (var resposta in perguntas[perguntaAtual].cast()["respostas"]) {
+      print(resposta);
+
+
+      respostas.add(
+          Resposta(resposta, acao)
+        );
+        
+    }
+
+}
+
+  
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Questao(perguntas[perguntaAtual]["texto"].toString()),
+        ),
+        body: Column(
+          children: [
+            ...respostas,
+          ],
+        ),
+      )
+    );
+
+
+
+    
+  }
+}
